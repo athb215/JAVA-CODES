6bbbae1e1965291231180b9b72c3ccf6fdb2608f
athb215 <atharvabhutkar0@gmail.com>
2025-10-15 14:03:35 +0530

    Add files via upload

---
+++ program19.java
@@ -0,0 +1,16 @@
+ class program19
+ {
+    public void Display()
+    {
+        System.out.println("jay ganesh..");
+        System.out.println("jay ganesh..");
+        System.out.println("jay ganesh..");
+        System.out.println("jay ganesh..");
+    }
+    public static void main(String A[])
+    {
+      Display();   //error
+        
+        
+    }
+}
