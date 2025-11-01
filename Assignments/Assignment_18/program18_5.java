292e47e69dd7e9f9f2ba95c45ae5326b9171c7be
Atharva Bhutkar <atharvabhutkar0@gmail.com>
2025-11-01 19:14:17 +0530

    Update

--- Assignments/Assignment_18/program18_5.java
+++ Assignments/Assignment_18/program18_5.java
@@ -7,7 +7,7 @@ class Logic
     //  Description   : To check the number is negative, positive or zero
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Atharva Kaustubh Bhutkar
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
