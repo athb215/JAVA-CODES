e0a2ade6172fc4a6e8675f81efeba73c03cabc4d
Atharva Bhutkar <atharvabhutkar0@gmail.com>
2025-11-01 19:17:31 +0530

    Update

--- Assignments/Assignment_20/program20_2.java
+++ Assignments/Assignment_20/program20_2.java
@@ -6,7 +6,7 @@ class Logic
     //  Description   : To Display N number in reverse 
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Atharva Kaustubh Bhutkar
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
